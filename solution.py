3028113fbdc69acfbc25b26aec8f23c4a6951932
Dilyara Fatykhova <72702419+diquid@users.noreply.github.com>
2023-04-08 03:04:58 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,8 @@ chat_id = 1234206085
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    loc = x.mean()
     maximum = x.max()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
 #     return loc - scale * norm.ppf(1 - alpha / 2), \
 #            loc - scale * norm.ppf(alpha / 2)
-    return (maximum - ((maximum-0.041)* norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x))),
-           maximum + ((maximum-0.041)* norm.ppf(1 - alpha / 2) / np.sqrt(2*len(x))))
+    return maximum, (maximum-0.041) / alpha ** (1/len(x)) + 0.041
+    
