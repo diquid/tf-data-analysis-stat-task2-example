898531dab93c6bce8019c850c0cef6a841cd6381
Dilyara Fatykhova <72702419+diquid@users.noreply.github.com>
2023-04-08 03:11:32 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,8 +8,5 @@ chat_id = 1234206085
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    maximum = x.max()
-#     return loc - scale * norm.ppf(1 - alpha / 2), \
-#            loc - scale * norm.ppf(alpha / 2)
-    return maximum, (maximum-0.041) / alpha ** (1/len(x)) + 0.041
-    
+    maximum = np.max(x)
+    return maximum, (maximum - 0.041) / alpha ** (1/len(x)) + 0.041
